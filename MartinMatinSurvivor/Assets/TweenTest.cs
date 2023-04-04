d665595bd1e06f5b420107533cb3a5d76b2017c8
EliSpar <Eli@leseum.com>
2023-04-04 09:34:57 +0200

    Tween marche

---
+++ MartinMatinSurvivor/Assets/TweenTest.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TweenTest : MonoBehaviour
+{
+    private Vector3 _originalScale;
+    private Vector3 _scaleTo;
+    void Start()
+    {
+        _originalScale = transform.localScale;
+        _scaleTo = _originalScale * 1.5f;
+
+
+        onScale();
+    }
+
+    private void onScale()
+    {
+        transform.DOScale(_scaleTo, .5f)
+            .SetEase(Ease.Flash)
+            .OnComplete(() =>
+            {
+
+                transform.DOScale(_originalScale, .5f)
+                .SetEase(Ease.OutElastic);
+
+            });
+
+    }
+ 
+}
